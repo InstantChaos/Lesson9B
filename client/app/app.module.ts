619811f65331a9e39b30018b0e01ee83ba3821de
Franco Chong <fnbeddie@hotmail.com>
2017-03-18 14:40:20 -0400

    fixed minor errors

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+// Here is a comment
+
 @NgModule({
   imports:      [BrowserModule],
   declarations: [AppComponent],
--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
-app.use(express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 
 
 // setup session
